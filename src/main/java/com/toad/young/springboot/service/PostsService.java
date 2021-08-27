b94ad576cf9f43d9353a45b8e18847808220d8e4
Youngtoad <youngjin033@gmail.com>
2021-08-27 13:59:19 +0900

    게시판 글 등록 기능 추가

--- src/main/java/com/toad/young/springboot/service/PostsService.java
+++ src/main/java/com/toad/young/springboot/service/PostsService.java
@@ -2,13 +2,17 @@ package com.toad.young.springboot.service;
 
 import com.toad.young.springboot.domain.posts.Posts;
 import com.toad.young.springboot.domain.posts.PostsRepository;
+import com.toad.young.springboot.web.dto.PostsListResponseDto;
 import com.toad.young.springboot.web.dto.PostsResponseDto;
 import com.toad.young.springboot.web.dto.PostsSaveRequestDto;
 import com.toad.young.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -29,9 +33,17 @@ public class PostsService {
         return id;
     }
 
+    @Transactional(readOnly = true)
     public PostsResponseDto findById (Long id){
         Posts entity = postsRepository.findById(id).orElseThrow(()->new IllegalArgumentException("해당 게시글이 없습니다. id="+ id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
